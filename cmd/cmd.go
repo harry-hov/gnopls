c13556d92430322d7df12c800571d24fd5d71b9b
Hariom Verma <hariom18599@gmail.com>
2023-09-22 06:13:21 +0530

    Init gnopls cmd using spf13/cobra

---
+++ cmd/cmd.go
@@ -0,0 +1,32 @@
+package cmd
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+func GnoplsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                "gnopls",
+		Short:              `Gno Please! is a Gno language server`,
+		DisableSuggestions: true,
+		SilenceUsage:       true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			slog.Info("Initializing Server...")
+
+			return nil
+		},
+	}
+
+	cmd.CompletionOptions.DisableDefaultCmd = true
+
+	return cmd
+}
+
+func Execute() {
+	if err := GnoplsCmd().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/harry-hov/gnopls/cmd"
+
+func main() {
+	cmd.Execute()
+}
